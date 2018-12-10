770d1956adda80c60e61eb63f4cb06787204f5d9
huhongsen <huhongsen@raycloud.com>
2018-12-10 21:19:00 +0800

    配置默认错误页面跳转.

--- src/main/java/com/learn/controller/IndexController.java
+++ src/main/java/com/learn/controller/IndexController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
+ * Controller注解支持页面跳转
+ * RestController注解仅支持数据返回.
+ *
  * @author huhongsen
  * @date 2018/12/10
  */
@@ -13,5 +16,15 @@ public class IndexController {
     @RequestMapping("/")
     public String index() {
         return "index.html";
+    }
+
+    @RequestMapping("/404")
+    public String error404() {
+        return "/error/404.html";
+    }
+
+    @RequestMapping("/405")
+    public String error405() {
+        return "/error/405.html";
     }
 }
---
+++ src/main/java/com/learn/registrar/ErrorPageRegistrarImpl.java
@@ -0,0 +1,23 @@
+package com.learn.registrar;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * 注册默认响应码映射页面
+ *
+ * @author huhongsen
+ * @date 2018/12/10
+ */
+@Component
+public class ErrorPageRegistrarImpl implements ErrorPageRegistrar {
+    @Override
+    public void registerErrorPages(ErrorPageRegistry errorPageRegistry) {
+        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
+        ErrorPage error405Page = new ErrorPage(HttpStatus.METHOD_NOT_ALLOWED, "/405");
+        errorPageRegistry.addErrorPages(error404Page, error405Page);
+    }
+}
