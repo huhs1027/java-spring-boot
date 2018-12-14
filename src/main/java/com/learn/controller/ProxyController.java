7a526445c942fcb354667545e351aa70e1eb013d
huhongsen <huhongsen@raycloud.com>
2018-12-14 14:14:31 +0800

    代理流 不特地处理图片. 直接返回

--- src/main/java/com/learn/controller/ProxyController.java
+++ src/main/java/com/learn/controller/ProxyController.java
@@ -1,20 +1,17 @@
 package com.learn.controller;
 
 import com.learn.entity.ProxyRequest;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.imageio.ImageIO;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 /**
@@ -34,10 +31,11 @@ public class ProxyController {
         HttpGet httpGet = new HttpGet(proxyRequest.getRequestUrl());
         HttpResponse httpResponse = httpClient.execute(httpGet);
 
-        httpServletResponse.setContentType(ContentType.IMAGE_JPEG.getMimeType());
-        ServletOutputStream outputStream = httpServletResponse.getOutputStream();
-        //写流
-        BufferedImage read = ImageIO.read(httpResponse.getEntity().getContent());
-        ImageIO.write(read, "jpg", outputStream);
+        //把代理相应流转成图片流
+//        BufferedImage read = ImageIO.read(httpResponse.getEntity().getContent());
+//        ImageIO.write(read, "jpg", httpServletResponse.getOutputStream());
+        //不处理直接返回代理相应流.
+        byte[] bytes = IOUtils.toByteArray(httpResponse.getEntity().getContent());
+        httpServletResponse.getOutputStream().write(bytes);
     }
 }
